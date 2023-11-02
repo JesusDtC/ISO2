1b3c842606d6412def2aafa4c5dc0cc96d0bf956
rubenrfm7 <127775042+rubenrfm7@users.noreply.github.com>
2023-11-02 22:40:17 +0100

    Implemtentacion y test It1

---
+++ it1/src/main/Server/RegistrarUsuario.java
@@ -0,0 +1,10 @@
+package ISO2_2023_B01.it1;
+
+public class RegistrarUsuario {
+	public void registrarUsuario{
+		
+	}
+	public void validarUsuario{
+		
+	}
+}
---
+++ it1/src/main/Server/Usuario.java
@@ -0,0 +1,15 @@
+package ISO2_2023_B01.it1;
+
+public class Usuario {
+
+	private String login;
+	private String contrasena;
+	private String tipoUsuario;
+
+	public Usuario(String login, String contrasena, String tipoUsuario) {
+	        this.login = login;
+	        this.contrasena = contrasena;
+	        this.tipoUsuario = tipoUsuario;
+	    }
+	
+}
