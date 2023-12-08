ad60f6c48907133e0b8a2c677a08bcb43e3d660f
rubenrfm7 <127775042+rubenrfm7@users.noreply.github.com>
2023-12-08 18:55:44 +0100

    Implementación y test 0.3.0asis

---
+++ it4/src/main/java/Asistente/ControlEvento.java
@@ -0,0 +1,28 @@
+package Asistente;
+
+
+public class ControlEvento {
+
+
+
+	public void buscarEvento() {
+		// TODO - implement ControlEvento.buscarEvento
+		throw new UnsupportedOperationException();
+	}
+
+	public void AñadirEventoCalend() {
+		// TODO - implement ControlEvento.mostrarInfo
+		throw new UnsupportedOperationException();
+	}
+
+	public void valorarEvento() {
+		// TODO - implement ControlEvento.planHorario
+		throw new UnsupportedOperationException();
+	}
+
+	public void RegistrarseEnEvento() {
+		// TODO - implement ControlEvento.RegistrarseEnEvento
+		throw new UnsupportedOperationException();
+	}
+
+}
