5b927fc738a8c651728bccd799cdacd3ef146593
werth2002 <114737693+werth2002@users.noreply.github.com>
2023-12-18 19:38:07 +0100

    Add files via upload
    Implementación de servidor hecha por José Javier e Iván Jiménez

---
+++ Servidor/src/main/Dominio/Valoracion.java
@@ -0,0 +1,11 @@
+package Server;
+
+public class Valoracion {
+	private int id;
+	private int puntuacion;
+	private String comentario;
+	private Evento evento;
+	private Usuario usuario;
+	private Boolean isPresencial;
+	private String tipoEvento;
+}
---
+++ Servidor/src/main/Persistencia/AgenteDB.java
@@ -0,0 +1,45 @@
+package Server;
+
+import java.sql.Connection;
+
+public class AgenteDB {
+
+	private Connection conexion;
+	private String usuarioBD;
+
+	public void getAgent() {
+		// TODO - implement AgenteDB.getAgent
+		throw new UnsupportedOperationException();
+	}
+
+	public void conectar() {
+		// TODO - implement AgenteDB.conectar
+		throw new UnsupportedOperationException();
+	}
+
+	public void desconectar() {
+		// TODO - implement AgenteDB.desconectar
+		throw new UnsupportedOperationException();
+	}
+
+	public void guardarUsuario() {
+		// TODO - implement AgenteDB.guardarUsuario
+		throw new UnsupportedOperationException();
+	}
+
+	public void eliminarUsuario() {
+		// TODO - implement AgenteDB.eliminarUsuario
+		throw new UnsupportedOperationException();
+	}
+
+	public void actualizarUsuario() {
+		// TODO - implement AgenteDB.actualizarUsuario
+		throw new UnsupportedOperationException();
+	}
+
+	public void obtenerUsuario() {
+		// TODO - implement AgenteDB.obtenerUsuario
+		throw new UnsupportedOperationException();
+	}
+
+}
