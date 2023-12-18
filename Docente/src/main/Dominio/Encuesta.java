bbe586ee1da3e834e0c1e70b22df794859444957
werth2002 <114737693+werth2002@users.noreply.github.com>
2023-12-18 19:40:01 +0100

    Add files via upload
    Implementación de Docente hecha por José Javier e Iván Jiménez

---
+++ Docente/src/main/Dominio/Encuesta.java
@@ -0,0 +1,23 @@
+
+public class Encuesta {
+	
+	int id;
+
+	public Encuesta(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void aceptar_escuesta() {
+		// TODO - implement Encuesta.aceptar_escuesta
+		throw new UnsupportedOperationException();
+	}
+	
+}
